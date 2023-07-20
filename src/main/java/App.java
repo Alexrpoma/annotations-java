d852f1824432a027cd755ab87257d9792b0e0b3c
Alex Poma <alex.rpoma@outlook.com>
2023-07-20 10:40:51 -0400

    frist commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+  public static void main(String[] args) {
+
+    //annotations examples:
+    @SuppressWarnings("unused")
+    List<String> list = new ArrayList<>();
+  }
+}
